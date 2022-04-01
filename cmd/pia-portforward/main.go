a92cf35a4093f44ecd9acb004afcf846d9df6989
Joel D. Elkins <joel@elkins.co>
2022-04-01 18:55:33 -0500

    Always honor -rtorrent if set

--- cmd/pia-portforward/main.go
+++ cmd/pia-portforward/main.go
@@ -17,65 +17,54 @@ var (
 	rtorrent_url string
 )
 
-func parse_args() {
+func parseArgs() {
 	const path_systemd_networkd = "/etc/systemd/network"
 	flag.StringVar(&wg_if, "ifname", "pia", "name of wireguard interface, used to determine cache filename")
 	flag.StringVar(&pia_username, "username", "", "PIA username")
 	flag.StringVar(&pia_password, "password", "", "PIA password")
-	flag.BoolVar(&refresh_only, "refresh", false, "Refresh active (cached) port assignment")
+	flag.BoolVar(&refresh_only, "refresh", false, "Refresh cached port assignment, rather than getting a new assignment")
 	flag.StringVar(&rtorrent_url, "rtorrent", "", "Notify rtorrent of the assigned port via XML-RPC at this URL")
 	flag.Parse()
 }
 
-func verify_port(tun *pia.Tunnel) int {
-	port, err := rtorrent.Confirm(rtorrent_url)
-	if err != nil {
-		log.Fatalf("Could not verify rtorrent port: %v", err)
-	}
-	if port != tun.PFSig.Port {
-		log.Fatalf("PIA assigned us port %d, but rtorrent reports port is %d", tun.PFSig.Port, port)
-	}
-	return port
-}
-
 func main() {
-	parse_args()
-	port_verified := false
+	parseArgs()
 	tun, err := pia.ReadCache(wg_if)
+	defer func() {
+		if err := tun.SaveCache(); err != nil {
+			log.Panicf("Could not save cache: %v", err)
+		}
+	}()
 	if err != nil {
-		log.Fatalf("Could not read cache: %v", err)
+		log.Panicf("Could not read cache: %v", err)
 	}
 	if !tun.Token.Valid() {
 		if pia_username == "" || pia_password == "" {
-			log.Fatalf("Token expired and user/pass not provided")
+			log.Panicf("Token expired and user/pass not provided")
 		}
 		if err := tun.NewToken(pia_username, pia_password); err != nil {
-			log.Fatalf("Token expired; error refreshing: %v", err)
+			log.Panicf("Token expired; error refreshing: %v", err)
 		}
 	}
 	if !refresh_only {
 		if err := tun.NewPFSig(); err != nil {
-			log.Fatalf("Could not get port forwarding signature: %v", err)
+			log.Panicf("Could not get port forwarding signature: %v", err)
 		}
-		if rtorrent_url != "" {
-			if err := rtorrent.Notify(rtorrent_url, tun.PFSig.Port); err != nil {
-				log.Fatalf("Could not notify rtorrent (at %s) of assigned port: %v", rtorrent_url, err)
-			}
-			verify_port(tun)
-			port_verified = true
-		}
-		defer func() {
-			if err := tun.SaveCache(); err != nil {
-				log.Fatalf("Could not save cache: %v", err)
-			}
-		}()
-	}
-	if rtorrent_url != "" && !port_verified {
-		verify_port(tun)
-		port_verified = true
 	}
 	if err := tun.BindPF(); err != nil {
-		log.Fatalf("Could not pind port forwarding assignment: %v", err)
+		log.Panicf("Could not pind port forwarding assignment: %v", err)
+	}
+	if rtorrent_url != "" {
+		if err := rtorrent.Notify(rtorrent_url, tun.PFSig.Port); err != nil {
+			log.Panicf("Could not notify rtorrent (at %s) of assigned port: %v", rtorrent_url, err)
+		}
+		port, err := rtorrent.Confirm(rtorrent_url)
+		if err != nil {
+			log.Panicf("Could not verify rtorrent port: %v", err)
+		}
+		if port != tun.PFSig.Port {
+			log.Panicf("PIA assigned us port %d, but rtorrent reports port is %d", tun.PFSig.Port, port)
+		}
 	}
 	fmt.Printf("%s: %s (Port = %d)\n", tun.Status, tun.Message, tun.PFSig.Port)
 }
