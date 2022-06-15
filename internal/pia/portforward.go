7fcf5ee315a98046a505f16909852c778dc29f36
Joel D. Elkins <joel@elkins.co>
2022-06-15 03:35:09 -0500

    Add a few comments

--- internal/pia/portforward.go
+++ internal/pia/portforward.go
@@ -16,6 +16,24 @@ type PortForwardSig struct {
 }
 
 func (tun *Tunnel) NewPFSig() error {
+	// Explanation of PIA's /getSignature endpoint
+	// You call this endpoint with a valid token, and it returns a json string
+	// encoding a dict with these fields:
+	// - status  : don't want to keep
+	// - message : don't want to keep
+	// - payload : want to keep
+	// Payload is where the important info (especially the assigned port) is,
+	// so we need to unpack it do do anything with it. Luckily, the payload
+	// field is a base64-encoded json string as well, with (at least) these
+	// fields:
+	// - port       : want to keep
+	// - expires_at : want to keep
+	// - signature  : want to keep
+	// I don't know exactly what "signature" is for, but we need to carry it to
+	// refresh the assginment.
+	// I use a kind of cute approach to store both the payload as well as the
+	// decoded contents of payload, all in one little struct, namely to unmarshal
+	// payload on top of its containing struct.
 	url := fmt.Sprintf("https://%s:19999/getSignature", tun.ServerVip)
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
@@ -29,6 +47,9 @@ func (tun *Tunnel) NewPFSig() error {
 		return err
 	}
 	defer resp.Body.Close()
+	// A disposable wrapper to grab the request status and any error message.
+	// We won't keep this info, just the stuff in PortForwardSig. For now,
+	// we'll be just getting PortForwardSig.Payload
 	var r struct {
 		Status  string `json:"status"`
 		Message string `json:"message"`
@@ -44,6 +65,7 @@ func (tun *Tunnel) NewPFSig() error {
 	if err != nil {
 		return err
 	}
+	// Payload's contained fields will now be brothers of Payload
 	if err := json.Unmarshal(payload_b, &r); err != nil {
 		return err
 	}
