593a2b414fed7e0001fedf4d8299d523e7124800
Joel D. Elkins <joel@elkins.co>
2025-04-24 10:59:52 -0500

    nix module: now close to working at mvp level
    TODO:
    - make options and configuration for automated porforward refreshing
    - make configuration for templates, perhaps as nixified systemd network
    unit definitions, or at least as file paths
    - figure out how to deal with runtime network unit changes in the nixos
    way. Dynamic nature of the link is a bit contrary to a purely
    declarative system.

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
     inherit (nixpkgs) lib;
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+
     pkg = pkgs.buildGoModule {
       pname = "pia-tools";
       version = "1.1.6";
@@ -18,48 +19,46 @@
         license = lib.licenses.mit;
       };
     };
-    module = with lib; with options; { config, pkgs, utils, ...}: {
+
+    module = with lib; with options; { config, pkgs, utils, ...}: let
+      cfg = config.pia-tools;
+    in {
       options.pia-tools = {
         enable = mkEnableOption "pia-tools";
         user = mkOption {
           description = "User to run the tool as.";
-          type = types.string;
+          type = types.str;
           default = "pia";
         };
         group = mkOption {
           description = "Group to run the tool as.";
-          type = types.string;
+          type = types.str;
           default = "pia";
         };
         ifname = mkOption {
           description = "Name of PIA WireGuard network interface";
-          type = types.string;
+          type = types.str;
           default = "wg_pia";
         };
         region = mkOption {
           description = "Region to connect to, or auto by default.";
-          type = types.string;
+          type = types.str;
           default = "auto";
         };
         rTorrentParams = mkOption {
           description = "Additional parameters to pia-setup-tunnel to connect to rTorrent";
-          type = types.string;
+          type = types.str;
           default = "";
         };
         transmissionParams = mkOption {
           description = "Additional parameters to pia-setup-tunnel to connect to Transmission bittorrent server";
-          type = types.string;
+          type = types.str;
           default = "";
-        };
-        timerEnabled = mkOption {
-          description = "Whether to enable a timer to periodically refresh the tunnel.";
-          type = types.bool;
-          default = true;
         };
         envFile = mkOption {
           description = ''
             Required. Path to file setting environment variables to be used
-            in setting up the tunnel device ${config.pia-tools.ifname}.
+            in setting up the tunnel device ${cfg.ifname}.
             The recognized variables are as follows. Read accompanying documentation
             and/or use ``${pkg}/bin/pia-setup-tunnel --help``.
 
@@ -70,12 +69,13 @@
         };
         serviceName = mkOption {
           description = "Name of systemd service for pia-tools tunnel refresh";
-          type = types.string;
-          default = "pia-refresh-${config.pia-tools.ifname}";
+          type = types.str;
+          default = "pia-refresh-${cfg.ifname}";
         };
         timerConfig = mkOption {
-          description = "Timer defining frequency of resetting the tunnel.";
-          type = utils.systemdUtils.types.timerConfig;
+          description = "Timer defining frequency of resetting the tunnel. Set to null to disable.";
+          type = with types; nullOr (attrsOf utils.systemdUtils.unitOptions.unitOption);
+          example = "null";
           default = {
             OnCalendar = "Wed *-*-* 03:00:00";
             RandomizedDelaySec = "72h";
@@ -83,40 +83,40 @@
         };
       };
 
-      config = lib.mkIf config.pia-tools.enable {
-        users.users.pia = lib.mkIf (config.pia-tools.user == "pia") {
+      config = lib.mkIf cfg.enable {
+        users.users.pia = lib.mkIf (cfg.user == "pia") {
           description = "pia-tools system user account";
           isSystemUser = true;
-          group = config.pia-tools.group;
+          group = cfg.group;
         };
-        users.groups.pia = lib.mkIf (config.pia-tools.group == "pia") {
-          members = [ config.pia-tools.user ];
+        users.groups.pia = lib.mkIf (cfg.group == "pia") {
+          members = [ cfg.user ];
         };
         systemd.tmpfiles.settings."50-pia"."/var/cache/pia".d = {
-          user = config.pia-tools.user;
-          group = config.pia-tools.group;
+          user = cfg.user;
+          group = cfg.group;
           mode = "0750";
         };
-        systemd.services.${config.pia-tools.serviceName} = {
-          name = "${config.pia-tools.serviceName}.service";
+        systemd.services.${cfg.serviceName} = {
+          name = "${cfg.serviceName}.service";
           serviceConfig = {
             Type = "oneshot";
-            EnvironmentFile = config.pia-tools.envFile;
-            ExecStart = "ExecStart=${pkg}/bin/pia-setup-tunnel --region ${config.pia-tools.region} --username $PIA_USERNAME --password $PIA_PASSWORD --ifname ${config.pia-tools.ifname}";
+            EnvironmentFile = cfg.envFile;
+            ExecStart = "${pkg}/bin/pia-setup-tunnel --region ${cfg.region} --username $PIA_USERNAME --password $PIA_PASSWORD --ifname ${cfg.ifname}";
             ExecStartPost = [
-              "-${pkgs.iproute2}/bin/ip link set down dev ${config.pia-tools.ifname}"
-              "-${pkgs.iproute2}/bin/ip link del ${config.pia-tools.ifname}"
+              "-${pkgs.iproute2}/bin/ip link set down dev ${cfg.ifname}"
+              "-${pkgs.iproute2}/bin/ip link del ${cfg.ifname}"
               "${pkgs.systemd}/bin/networkctl reload"
-              "${pkgs.systemd}/bin/networkctl reconfigure ${config.pia-tools.ifname}"
-              "${pkgs.systemd}/bin/networkctl up ${config.pia-tools.ifname}"
+              "${pkgs.systemd}/bin/networkctl reconfigure ${cfg.ifname}"
+              "${pkgs.systemd}/bin/networkctl up ${cfg.ifname}"
               "${pkgs.coreutils}/bin/sleep 10"
-              "-${pkg}/bin/pia-portforward --ifname ${config.pia-tools.ifname} ${config.pia-tools.rTorrentParams} ${config.pia-tools.transmissionParams}"
+              "-${pkg}/bin/pia-portforward --ifname ${cfg.ifname} ${cfg.rTorrentParams} ${cfg.transmissionParams}"
             ];
           };
         };
-        systemd.timers.${config.pia-tools.serviceName} = lib.mkIf config.pia-tools.timerEnabled {
-          name = "${config.pia-tools.serviceName}.timer";
-          timerConfig = config.pia-tools.timerConfig;
+        systemd.timers.${cfg.serviceName} = lib.mkIf (cfg.timerConfig != null) {
+          name = "${cfg.serviceName}.timer";
+          timerConfig = cfg.timerConfig;
           wantedBy = [ "timers.target" ];
         };
       };
