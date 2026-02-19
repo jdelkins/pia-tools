5cf95ff290bc1ea9612b4a2b5cd35971ff324152
Joel D. Elkins <joel@elkins.co>
2026-02-19 11:20:07 -0600

    Capabilities alone don't allow creating files
    To work properly, and also write to /etc/systemd/netowrk, the app needs
    to run as root. If you don't like this, you can have it write to
    /var/cache/pia (using --net{work,dev}-file=output=/var/cache/pia/...),
    and then add an ExecStartPost="+install ..." step to copy the files into
    place, which could arguably be a TOCTOU issue, but a common and
    manageable pattern.

--- module.nix
+++ module.nix
@@ -229,13 +229,9 @@ in
         UMask = "0002";
         CapabilityBoundingSet = [
           "CAP_NET_ADMIN"
-          "CAP_CHOWN"
-          "CAP_FOWNER"
         ];
         AmbientCapabilities = [
           "CAP_NET_ADMIN"
-          "CAP_CHOWN"
-          "CAP_FOWNER"
         ];
         ProtectSystem = "strict";
         ProtectHome = true;
@@ -275,7 +271,7 @@ in
           "AF_UNIX"
           "AF_NETLINK"
         ];
-        ExecStart = ''${cfg.package}/bin/pia-setup-tunnel --wg-binary ${pkgs.wireguard-tools}/bin/wg --cache-dir ${cfg.cacheDir} --region ${cfg.region} --if-name ${cfg.ifname} --netdev-file="template=${cfg.netdevTemplateFile},output=${cfg.netdevFile},group=systemd-network,mode=0440" --network-file="template=${cfg.networkTemplateFile},output=${cfg.networkFile},mode=0444"'';
+        ExecStart = ''+${cfg.package}/bin/pia-setup-tunnel --wg-binary ${pkgs.wireguard-tools}/bin/wg --cache-dir ${cfg.cacheDir} --region ${cfg.region} --if-name ${cfg.ifname} --netdev-file="template=${cfg.netdevTemplateFile},output=${cfg.netdevFile},group=systemd-network,mode=0440" --network-file="template=${cfg.networkTemplateFile},output=${cfg.networkFile},mode=0444"'';
         ExecStartPost = [
           "-${pkgs.iproute2}/bin/ip link set down dev ${cfg.ifname}"
           "-${pkgs.iproute2}/bin/ip link del ${cfg.ifname}"
